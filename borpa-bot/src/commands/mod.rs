e40c36abe473210e248d73e0864e35c0bcffbd3a
Dominic Harris <dominic@domm.me>
2021-10-23 11:00:27 -0400

    add a todo

--- borpa-bot/src/commands/mod.rs
+++ borpa-bot/src/commands/mod.rs
@@ -7,6 +7,7 @@ use borpa_commands::command::Command;
 pub fn get_commands() -> Vec<Arc<Command>> {
     let mut commands: Vec<Arc<Command>> = vec![];
 
+    // TODO: Clean up defining commands by making a macro to help
     let ping = Arc::new(Command {
         name: "ping".to_string(),
         description: "Get bots ping".to_string(),
@@ -15,6 +16,6 @@ pub fn get_commands() -> Vec<Arc<Command>> {
         handler: Box::new(move |ctx| Box::pin(misc::ping::ping(ctx))),
     });
     commands.push(ping);
-    
+
     commands
 }
