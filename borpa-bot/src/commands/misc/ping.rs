f834fffd9683a1f76e1f56540b871a42cf5139ad
Dominic Harris <dominic@domm.me>
2021-10-26 09:32:34 -0400

    (git) Add CI

--- borpa-bot/src/commands/misc/ping.rs
+++ borpa-bot/src/commands/misc/ping.rs
@@ -1,6 +1,6 @@
 use borpa_commands::{command::CommandReturn, context::Context};
 
-pub async fn ping(ctx: Context) -> CommandReturn {
+pub async fn ping(_ctx: Context) -> CommandReturn {
     println!("Ping command called");
 
     Ok(())
